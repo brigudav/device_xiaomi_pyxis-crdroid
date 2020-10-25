8a560dc2dbe55a717d0421420d7a7b394ee35d20
brigudav <brigudav@gmail.com>
2020-10-25 21:22:38 +1000

    init-tree

---
+++ lineage_pyxis.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from pyxis device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := pyxis
+PRODUCT_NAME := lineage_pyxis
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 9 Lite
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="pyxis" \
+    TARGET_DEVICE="pyxis"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
